3fc6e8ab4e800fe1a2dabca40769384fc906e557
JannatulHappy <jannatulferdoushappy21@gmail.com>
2022-05-21 23:53:52 +0600

    mongooseConnected

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+const app = express();
+import dotenv from "dotenv";
+app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+dotenv.config();
+const port = 5000;
+const MONGODB_URL = process.env.MONGODB_URL;
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+      app.get("/",(req,res)=>{
+          res.send("Hello from Aventura Server")
+      })
+    app.listen(port, () => {
+      console.log(`Server running on Port ${port}.`);
+    });
+  })
+  .catch((error) => {
+    console.log(`${error} did not connect!!`);
+  });
